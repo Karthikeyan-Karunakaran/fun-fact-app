1057f8d1e1c43e146c3b8378a82051ff7426d5cb
Karthikeyan Karunakaran <93427303+Karthikeyan-Karunakaran@users.noreply.github.com>
2025-03-13 14:27:49 +0530

    Update fun_fact_generator.py

--- fun_fact_generator.py
+++ fun_fact_generator.py
@@ -1,3 +1,18 @@
+# Import the necessary modules
+import json
+import requests
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import *
+from pywebio import start_server
+
+def get_fun_fact(_):
+    # Clear the screen
+    clear()
+
+    # Put HTML content for the fun fact generator header
+    put_html(
+        '<p align="left">'
         '<h2><img src="https://media.geeksforgeeks.org/wp-content/uploads/20210720224119/MessagingHappyicon.png" width="7%"> Fun Fact Generator</h2>'
         '</p>'
     )
